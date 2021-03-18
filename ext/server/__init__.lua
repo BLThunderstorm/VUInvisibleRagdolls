9ca6e390806e691751ddfc91474a62a940695e2c
Nefomemes <nefothingy@hotmail.com>
2021-03-18 09:20:00 +0700

    Update __init__.lua

--- ext/server/__init__.lua
+++ ext/server/__init__.lua
@@ -3,8 +3,7 @@ Events:Subscribe('Player:Killed', function(player)
     local rawCorpse = player.corpse or player.soldier;
     
     if(rawCorpse != nil) then
-    local corpse = SoldierEntity(rawCorpse);
-            
-            corpse.forceInvisible = true;
+    local corpse = SoldierEntity(rawCorpse);      
+    corpse.forceInvisible = true;
     not
 end)
