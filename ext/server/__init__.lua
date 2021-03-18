18dd055ccc5627f1c2e9246415285aa7e1534265
Nefomemes <nefothingy@hotmail.com>
2021-03-18 09:19:05 +0700

    Update __init__.lua

--- ext/server/__init__.lua
+++ ext/server/__init__.lua
@@ -1,4 +1,8 @@
 
-Events:Subscribe('Player:Killed', function(player, inflictor, position, weapon, isRoadKill, isHeadShot, wasVictimInReviveState, info)
-  
+Events:Subscribe('Player:Killed', function(player)
+    local rawCorpse = player.corpse or player.soldier;
+    
+    if(rawCorpse != nil) then
+    local corpse = SoldierEntity(player.soldier or player
+    not
 end)
